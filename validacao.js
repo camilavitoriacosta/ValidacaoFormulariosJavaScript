06836131df5b2ff7bd3cf6e53285029cd25bdc4a
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2022-02-23 10:40:21 -0400

    Adicionadando validação no campo data de nascimento

--- validacao.js
+++ validacao.js
@@ -8,14 +8,14 @@ function validaDataNascimento(input){
     const dataRecebida = new Date(input.value)
     var mensagem = ''
     
-    if(menorQueDiaAtual){
+    if(maiorQueDiaAtual(dataRecebida)){
         mensagem = "A data de nascimento precisa ser menor que a data atual"
     }
 
     input.setCustomValidity(mensagem)
 }
 
-function menorQueDiaAtual(data){
+function maiorQueDiaAtual(data){
     const dataAtual = new Date()
-    return data < dataAtual;
+    return data > dataAtual;
 }
